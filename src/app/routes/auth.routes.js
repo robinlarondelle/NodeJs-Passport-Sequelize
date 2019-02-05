1b6354603da04d16ea865f80936cd7f55e750dae
Robin La Rondelle <ra.larondelle@student.avans.nl>
2019-02-05 10:07:17 +0100

    Added Signin page

--- src/app/routes/auth.routes.js
+++ src/app/routes/auth.routes.js
@@ -2,4 +2,5 @@ var authController = require('../controllers/auth.controller');
  
 module.exports = function(app) {
     app.get('/signup', authController.signup);
+    app.get('/signin', authController.signin);
 }
